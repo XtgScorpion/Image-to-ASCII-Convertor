9958420448f402059b48c03cbfa26104a58dde5e
krish338 <krishpatel338@gmail.com>
2021-04-21 19:52:01 -0400

    Cleaned up code

--- conv.py
+++ conv.py
@@ -1,24 +1,41 @@
-import PIL.Image
+import PIL.Image 
 import math
+import requests
+import sys
 
-ramp1=list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1[]?-_+~<>i!lI;:,. ")
-ramp2=list(" .:-=+*#%@")
-ramp2.reverse()
+# Your ASCII Ramp sort on darkest to lightest characters
+toascii = list(reversed(list(" .:-=+*#%@")))
+# Grab the file path
+option = input("Where is your image from (local or web)? ")
+path = input("Image Location: ")
 
-path=input('Image Location: ')
-toascii=ramp2
-image=PIL.Image.open(path)
-w,h=image.size
-r=h/w
-nw=100
-nh=int(nw*r)
-image=(image.resize((nw,nh)))
-image=image.convert('L')
-imgvals=PIL.Image.Image.getdata(image)
-boxLen=int(255/len(toascii))
-print(nw,nh,len(imgvals), type(imgvals),boxLen)
-imgvals2=''.join([toascii[int(a/boxLen)-1] for a in imgvals])
-asciiimg="\n".join([imgvals2[a:(a+nw)] for a in range(0, len(imgvals2), nw)])
-print('a')
-f = open("ASCIIConverter\output.txt","w")
-f.write(asciiimg)
+# Open the file and extract the width and height
+if option == "local":
+    image = PIL.Image.open(path)
+elif option == "web":
+    image = PIL.Image.open(requests.get(path, stream=True).raw)
+
+w,h = image.size
+# Get the new dimensions with a max width/height of 100
+if w > h:
+    r = h/w
+    nw = 300
+    nh = int(nw*r)
+else:
+    r = w/h 
+    nh = 300
+    nw = int(nh*r)
+
+# Resize the image, convert to grayscale, and get the data in a flattened 2d array
+image = image.resize((nw,nh))
+image = image.convert('L')
+imgVals = PIL.Image.Image.getdata(image)
+
+# Change each grayscale pixel to its ASCII ramp equivelent and combine with newlines
+boxLen = int(255/len(toascii))
+imgVals2 = ''.join([toascii[int(a/boxLen)-1] for a in imgVals])
+asciiImg = "\n".join([imgVals2[a:(a+nw)] for a in range(0, len(imgVals2), nw)])
+
+# Write the ASCII Image to a text file
+f = open(sys.path[0]+"\output.txt","w")
+f.write(asciiImg)
